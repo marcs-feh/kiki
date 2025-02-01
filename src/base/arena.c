15023d391807cc9f571e65953dea0f9f3da2ebd9
marcs-feh <marcosfehlauer@protonmail.com>
2025-02-01 14:46:35 -0300

    mm

--- src/base/arena.c
+++ src/base/arena.c
@@ -14,7 +14,6 @@ bool arena_init_buffer(Arena* a, U8* data, Size len){
 	return true;
 }
 
-
 Uintptr arena_required_mem(Uintptr cur, Size count, Size align){
 	ensure(mem_valid_alignment(align), "Alignment must be a power of 2");
 	Uintptr aligned  = align_forward_ptr(cur, align);
